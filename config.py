2c0743382c0bc4a131cc551090279b4602baa89a
Wanjiru393 <carolciru393@gmail.com>
2022-05-14 01:57:09 +0300

    Tweaked file structure

--- config.py
+++ config.py
@@ -2,31 +2,76 @@ import os
 
 
 class Config:
-    """Parent Config Class """
-    SECRET_KEY = os.urandom(234102)
-    #  email configurations
+    '''
+    Parent config class
+    '''
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'SECRET_KEY'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://fkjcprkpdmtgke:3da620cc3546cef8066ff0bd1335445ece84cb858b5344919318c4bd121df546@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d6ab7t3gmljed8'
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get(
+    #     'DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Mail confugurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "cirusamuels@gmail.com"
-    MAIL_PASSWORD = "Ninja102"
-    
-    # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SUBJECT_PREFIX = 'PITCH APP!'
+    SENDER_EMAIL = 'cirusamuels@gmail.com'
 
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
-    """Production Config Class"""
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://fkjcprkpdmtgke:3da620cc3546cef8066ff0bd1335445ece84cb858b5344919318c4bd121df546@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d6ab7t3gmljed8'
+
+
+# SQLALCHEMY_DATABASE_URI = os.environ.get(
+#     'DATABASE_URL').replace("://", "ql://", 1)
+
+
+DEBUG = True
+
+
+class TestConfig(Config):
+    '''
+    Test
+    '''
+
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://fkjcprkpdmtgke:3da620cc3546cef8066ff0bd1335445ece84cb858b5344919318c4bd121df546@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d6ab7t3gmljed8'
+
+# SQLALCHEMY_DATABASE_URI = os.environ.get(
+#     'DATABASE_URL').replace("://", "ql://", 1)
 
 
 class DevConfig(Config):
-    """Developement Config"""
-    DEBUG = True
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+
+SQLALCHEMY_DATABASE_URI= 'postgresql://fkjcprkpdmtgke:3da620cc3546cef8066ff0bd1335445ece84cb858b5344919318c4bd121df546@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d6ab7t3gmljed8'
+
+# SQLALCHEMY_DATABASE_URI = os.environ.get(
+#     'DATABASE_URL').replace("://", "ql://", 1)
 
 
 config_options = {
     'development': DevConfig,
-    'production': ProdConfig
+    'production': ProdConfig,
+    'test': TestConfig,
 }
--- run.py
+++ run.py
@@ -1,172 +1,187 @@
-from flask import Flask,render_template ,flash
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField, PasswordField,BooleanField,ValidationError
-from wtforms.validators import  DataRequired,EqualTo,Length
-from werkzeug.security import generate_password_hash,check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from flask_migrate import Migrate
-from flask_login import UserMixin
-from wtforms.widgets import TextArea
+# from flask import Flask,render_template ,flash
+# from flask_wtf import FlaskForm
+# from wtforms import StringField,SubmitField, PasswordField,BooleanField,ValidationError
+# from wtforms.validators import  DataRequired,EqualTo,Length
+# from werkzeug.security import generate_password_hash,check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
+# from flask_migrate import Migrate
+# from flask_login import UserMixin,login_user,LoginManager,login_required,logout_user,current_user
+# from wtforms.widgets import TextArea
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-#Add Database #SQL
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# #Add Database #SQL
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
-#Mysql
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fkjcprkpdmtgke:3da620cc3546cef8066ff0bd1335445ece84cb858b5344919318c4bd121df546@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d6ab7t3gmljed8'
-# 'mysql+pymysql://root:password123@localhost/our_users'
+# #Mysql
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fkjcprkpdmtgke:3da620cc3546cef8066ff0bd1335445ece84cb858b5344919318c4bd121df546@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d6ab7t3gmljed8'
+# # 'mysql+pymysql://root:password123@localhost/our_users'
+# app.config ['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
-#Secret Key
-app.config['SECRET_KEY'] = "secretkeypass"
+# #Secret Key
+# app.config['SECRET_KEY'] = "secretkeypass"
 
-#Initialize The DataBase
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# #Initialize The DataBase
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
 
 
-#Pitch Model
-class Pitches(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250))
-    content = db.Column(db.Text)
-    author = db.Column(db.String())
-    date_posted = db.Column(db.DateTime, default=datetime)
+# #Pitch Model
+# class Pitches(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(250))
+#     content = db.Column(db.Text)
+#     author = db.Column(db.String())
+#     date_posted = db.Column(db.DateTime, default=datetime)
     
 
-#Pitch Form
-class PitchForm(FlaskForm):
-    title = StringField("Title",validators=[DataRequired()])
-    content = StringField("Content", validators=[DataRequired()], widget=TextArea())
-    author = StringField("Author", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+# #Pitch Form
+# class PitchForm(FlaskForm):
+#     title = StringField("Title",validators=[DataRequired()])
+#     content = StringField("Content", validators=[DataRequired()], widget=TextArea())
+#     author = StringField("Author", validators=[DataRequired()])
+#     submit = SubmitField("Submit")
 
 
-@app.route('/pitches')
-def pitches():
-    pitches= Pitches.query.order_by(Pitches.date_posted)
+# @app.route('/pitches')
+# def pitches():
+#     #Grabbing all posts
+#     pitches= Pitches.query.order_by(Pitches.date_posted)
 
-    return render_template("pitches.html",pitches=pitches)
+#     return render_template("pitches.html",pitches=pitches)
 
-#Post Page
-@app.route('/add-pitch', methods=['GET','POST'])
-def add_pitch():
-    form = PitchForm()
 
-    if form.validate_on_submit():
-        pitch = Pitches(title=form.title.data, content=form.content.data,
-        author=form.author.data
-        )
-        #Clear form
-        form.title.data = ''
-        form.content.data = ''
-        form.author.data = ''
+
+
+# @app.route('/pitches/<int:id>')
+# def pitch(id):
+#     pitch = Pitches.query.get_or_404(id)
+#     return render_template('pitch.html', pitch=pitch)
+
+
+
+# #Post Page
+# @app.route('/add-pitch', methods=['GET','POST'])
+# def add_pitch():
+#     form = PitchForm()
+
+#     if form.validate_on_submit():
+#         pitch = Pitches(title=form.title.data, content=form.content.data,
+#         author=form.author.data
+#         )
+#         #Clear form
+#         form.title.data = ''
+#         form.content.data = ''
+#         form.author.data = ''
         
-        #Add Pitch DataBase
-        db.session.add(pitch)
-        db.session.commit()
+#         #Add Pitch DataBase
+#         db.session.add(pitch)
+#         db.session.commit()
 
-        flash("Pitch Added Successfully!")
+#         flash("Pitch Added Successfully!")
 
-    return render_template("add_pitch.html",
-    form=form)
+#     return render_template("add_pitch.html",
+#     form=form)
 
 
 
-class Users(db.Model,UserMixin):
-    #Defined tablename
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(150), nullable=False, unique=True)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+# class Users(db.Model,UserMixin):
+#     #Defined tablename
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20),nullable=False, unique=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(150), nullable=False, unique=True)
+#     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
-    #Passwords
-    password_hash = db.Column(db.String(128))
+#     #Passwords
+#     password_hash = db.Column(db.String(128))
 
-    @property
-    def password(self):
-        raise AttributeError('Password is not areadable attribute!')
+#     @property
+#     def password(self):
+#         raise AttributeError('Password is not areadable attribute!')
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+#     @password.setter
+#     def password(self, password):
+#         self.password_hash = generate_password_hash(password)
     
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
+#     def verify_password(self, password):
+#         return check_password_hash(self.password_hash, password)
 
 
-      #Create String
-    def __repr__(self):
-        return '<Name %r>' % self.name
+#       #Create String
+#     def __repr__(self):
+#         return '<Name %r>' % self.name
 
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    user_to_delete =Users.query.get_or_404(id)
-    name = None
-    form = UserForm()
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     user_to_delete =Users.query.get_or_404(id)
+#     name = None
+#     form = UserForm()
 
-    try:
-        db.session.delete(user_to_delete)
-        db.session.commit()
-        flash("User Deleted Successfully")
+#     try:
+#         db.session.delete(user_to_delete)
+#         db.session.commit()
+#         flash("User Deleted Successfully")
 
-        our_users = Users.query.order_by(Users.date_added)
-        return render_template("add_user.html",
-        form=form,
-        name=name,
-        our_users=our_users)
+#         our_users = Users.query.order_by(Users.date_added)
+#         return render_template("add_user.html",
+#         form=form,
+#         name=name,
+#         our_users=our_users)
 
 
-    except:
-        flash("Delete Failed!!")
-        return render_template("add_user.html",
-        form=form,
-        name=name,
-        our_users=our_users)
+#     except:
+#         flash("Delete Failed!!")
+#         return render_template("add_user.html",
+#         form=form,
+#         name=name,
+#         our_users=our_users)
 
   
 
 
-class UserForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired()])
-    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2',message='Password should match')])
-    password_hash2 =PasswordField('Confirm password', validators=[DataRequired()])
-    submit = SubmitField("Submit")
+# class UserForm(FlaskForm):
+#     name = StringField("Name", validators=[DataRequired()])
+#     username = StringField("Username", validators=[DataRequired()])
+#     email = StringField("Email", validators=[DataRequired()])
+#     password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2',message='Password should match')])
+#     password_hash2 =PasswordField('Confirm password', validators=[DataRequired()])
+#     submit = SubmitField("Submit")
 
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+# @app.route("/")
+# def hello_world():
+#     return render_template('index.html')
 
-@app.route('/user/add', methods=['GET','POST'])
-def add_user():
-    name = None
-    form = UserForm()
-    if form.validate_on_submit():
-        user = Users.query.filter_by(email=form.email.data).first()
-        if user is None:
-            hashed_pw = generate_password_hash(form.password_hash.data)
-            user = Users(name=form.name.data, email=form.email.data, password_hash=hashed_pw)
-            db.session.add(user)
-            db.session.commit()
-        name = form.name.data
-        form.name.data = ''
-        form.email.data = ''
-        form.password_hash = ''
-        flash("User Added Successfully!")
-    our_users =Users.query.order_by(Users.date_added)
+# @app.route('/user/add', methods=['GET','POST'])
+# def add_user():
+#     name = None
+#     form = UserForm()
+#     if form.validate_on_submit():
+#         user = Users.query.filter_by(email=form.email.data).first()
+#         if user is None:
+#             hashed_pw = generate_password_hash(form.password_hash.data)
+#             user = Users(name=form.name.data, username=form.username.data, email=form.email.data, password_hash=hashed_pw)
+#             db.session.add(user)
+#             db.session.commit()
+#         name = form.name.data
+#         form.name.data = ''
+#         form.username.data = ''
+#         form.email.data = ''
+#         form.password_hash = ''
+#         flash("User Added Successfully!")
+#     our_users =Users.query.order_by(Users.date_added)
 
-    return render_template("add_user.html",
-    form=form,
-    name=name,
-    our_users=our_users)
+#     return render_template("add_user.html",
+#     form=form,
+#     name=name,
+#     our_users=our_users)
 
 
 
